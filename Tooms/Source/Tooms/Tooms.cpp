91186bb2c23fa8dfc73efe94e5f5586e3d97b367
Bryan Shumway <shumwaybryanm@gmail.com>
2019-11-27 21:31:52 -0800

    Removed Saved and Intermediate

--- Tooms/Source/Tooms/Tooms.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Tooms.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Tooms, "Tooms" );
